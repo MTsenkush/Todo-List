1e30c1da876a7d27bd1de46cbb2bf96c0a223cf8
Mariia Tsenkush <239046230+MTsenkush@users.noreply.github.com>
2026-09-01 22:48:50 -0500

    todoReducer action types defined, single initial state object created

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,40 @@
+export const TODO_ACTIONS = {
+  // Fetch operations
+  FETCH_START: 'FETCH_START',
+  FETCH_SUCCESS: 'FETCH_SUCCESS',
+  FETCH_ERROR: 'FETCH_ERROR',
+  
+  // Add todo operations
+  ADD_TODO_START: 'ADD_TODO_START',
+  ADD_TODO_SUCCESS: 'ADD_TODO_SUCCESS',
+  ADD_TODO_ERROR: 'ADD_TODO_ERROR',
+
+  // Complete todo operations
+  COMPLETE_TODO_START: 'COMPLETE_TODO_START',
+  COMPLETE_TODO_SUCCESS: 'COMPLETE_TODO_SUCCESS',
+  COMPLETE_TODO_ERROR: 'COMPLETE_TODO_ERROR',
+
+  // Update todo operations
+  UPDATE_TODO_START: 'UPDATE_TODO_START',
+  UPDATE_TODO_SUCCESS: 'UPDATE_TODO_SUCCESS',
+  UPDATE_TODO_ERROR: 'UPDATE_TODO_ERROR',
+
+  // UI operations
+  SET_SORT: 'SET_SORT',
+  SET_FILTER: 'SET_FILTER',
+  CLEAR_ERROR: 'CLEAR_ERROR',
+  CLEAR_FILTER_ERROR: 'CLEAR_FILTER_ERROR',
+  RESET_FILTERS: 'RESET_FILTERS',
+  INCREMENT_DATA_VERSION: 'INCREMENT_DATA_VERSION',
+};
+
+export const initialTodoState = {
+  todoList: [],
+  error: '',
+  filterError: '',
+  isTodoListLoading: true,
+  sortBy: 'createdAt',
+  sortDirection: 'desc',
+  filterTerm: '',
+  dataVersion: 0,
+};
