43b716b557a74c29d547a430e24896f81a220230
Mariia Tsenkush <239046230+MTsenkush@users.noreply.github.com>
2026-09-14 22:10:03 -0500

    Footer fixed

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-sky-100 shadow text-center py-6 w-full mt-auto">
+    <footer className="bg-sky-100 shadow text-center py-4 w-full mt-auto">
       © 2026 Mariia Tsenkush
     </footer>
   );
